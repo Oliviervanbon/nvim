23b2ded09212c5effe4c97e290c77deb5695e461
Olivier van Bon <olivier.vanbon@stcorp.nl>
2024-01-12 16:16:06 +0100

    Added colorcolumn=120 as python guideline

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,29 +1,29 @@
 local options = {
-    number = true,            -- show line numbers
-    relativenumber = false,    -- 
-    ignorecase = true,
-    smartcase = true,
-    incsearch = true,
-    autoindent = true,
-    cursorline = true,
-    cursorlineopt = "number",
-    tabstop = 2,
-    softtabstop = 0,
-    shiftwidth = 2,
-    expandtab = true,
-    smarttab = true,
-    wildmode = "longest,list",
-    backup = false,
-    cmdheight = 2,
-    hlsearch = true,
-    showmatch = true,
-    clipboard = "unnamedplus",
-    termguicolors = true
+	number = true, -- show line numbers
+	relativenumber = false, --
+	ignorecase = true,
+	smartcase = true,
+	incsearch = true,
+	autoindent = true,
+	cursorline = true,
+	cursorlineopt = "number",
+	tabstop = 2,
+	softtabstop = 0,
+	shiftwidth = 2,
+	expandtab = true,
+	smarttab = true,
+	wildmode = "longest,list",
+	backup = false,
+	cmdheight = 2,
+	hlsearch = true,
+	showmatch = true,
+	clipboard = "unnamedplus",
+	termguicolors = true,
+	colorcolumn = "120",
 }
 
 for k, v in pairs(options) do
-    vim.opt[k] = v
+	vim.opt[k] = v
 end
 
 vim.g.mapleader = "<"
-
