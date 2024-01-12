2c62812010d45d7768876e2dfbe88e082b7c4b20
Olivier van Bon <olivier.vanbon@stcorp.nl>
2024-01-12 11:23:17 +0100

    Add linting and formatting plugin none-ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,13 +1,13 @@
 return {
   {
     "williamboman/mason.nvim",
-    opts = {}
+    opts = {},
   },
   {
     "williamboman/mason-lspconfig.nvim",
     opts = {
-      ensure_installed = { "lua_ls", "jedi_language_server" }
-    }
+      ensure_installed = { "lua_ls", "ruff_lsp" }, -- "jedi_language_server"
+    },
   },
   {
     "neovim/nvim-lspconfig",
@@ -15,10 +15,36 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
-      lspconfig.jedi_language_server.setup({})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+      -- lspconfig.jedi_language_server.setup({})
+      lspconfig.ruff_lsp.setup({})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = {
+      ensure_installed = { "stylua", "ruff" }, -- "isort"
+    },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    opts = {},
+    config = function()
+      local null_ls = require("null-ls")
+
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.diagnostics.ruff,
+          null_ls.builtins.formatting.ruff,
+          -- Check if ruff also does import sorting, otherwise include:
+          -- null_ls.builtins.formatting.isort,
+        },
+      })
+
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+  },
 }
