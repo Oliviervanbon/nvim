e4a914bd5ffbfb4aa2e333f5b9217ccd65d825ce
Olivier van Bon <olivier.vanbon@stcorp.nl>
2024-01-12 16:08:50 +0100

    Remove duplicate nvim-cmp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -50,7 +50,7 @@ return {
         },
       })
 
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+      vim.keymap.set("n", "<leader>fc", vim.lsp.buf.format, {})
     end,
   },
 }
