69e436172fae806f546811a1d07d0522038c5576
Olivier van Bon <olivier.vanbon@stcorp.nl>
2024-01-12 11:57:50 +0100

    Add completion and snippets

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -13,10 +13,16 @@ return {
     "neovim/nvim-lspconfig",
     opts = {},
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
       -- lspconfig.jedi_language_server.setup({})
-      lspconfig.ruff_lsp.setup({})
+      lspconfig.ruff_lsp.setup({
+        capabilities = capabilities
+      })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
