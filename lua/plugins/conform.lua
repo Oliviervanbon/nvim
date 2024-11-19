632022fa34373fe60093147163e45d8e050b3f77
Olivier van Bon <olivier.vanbon@stcorp.nl>
2024-11-19 10:49:29 +0100

    Add nix lspconfig

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -72,6 +72,28 @@ return {
         },
       }
 
+      lspconfig.nixd.setup({
+        cmd = { "nixd" },
+        settings = {
+          nixd = {
+            nixpkgs = {
+              expr = "import <nixpkgs> { }",
+            },
+            formatting = {
+              command = { "nixfmt" },
+            },
+            options = {
+              nixos = {
+                expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.k-on.options',
+              },
+              home_manager = {
+                expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."ruixi@k-on".options',
+              },
+            },
+          },
+        },
+      })
+
       -- configure html server
       lspconfig.html.setup({
         capabilities = capabilities,
