566b8aa2fa51f40f70d3a2073e2abea934b4fb2f
Olivier van Bon <olivier.vanbon@stcorp.nl>
2024-12-19 14:26:45 +0100

    Various small updates

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -20,16 +20,11 @@ return {
 
       require("telescope").load_extension("fzf")
 
-      vim.keymap.set("n", "<space>fh", require("telescope.builtin").help_tags)
-      vim.keymap.set("n", "<space>fd", require("telescope.builtin").find_files)
-      vim.keymap.set("n", "<space>en", function()
+      vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
+      vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
+      vim.keymap.set("n", "<leader>en", function()
         require("telescope.builtin").find_files {
           cwd = vim.fn.stdpath("config")
-        }
-      end)
-      vim.keymap.set("n", "<space>ep", function()
-        require("telescope.builtin").find_files {
-          cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
         }
       end)
 
