12e8ebb84a0045c023e0cd7c0b7db6e6353793c2
Olivier van Bon <olivier.van.bon@outlook.com>
2024-12-28 10:56:23 +0100

    Add comment for telescope fzf native install

--- after/ftplugin/nix.lua
+++ after/ftplugin/nix.lua
@@ -1,6 +1,6 @@
 local set = vim.opt_local
 
-set.colorcolumn = "50"
+set.colorcolumn = "100"
 set.tabstop = 2
 set.softtabstop = 0
 set.shiftwidth = 2
--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -4,7 +4,11 @@ return {
     tag = '0.1.8',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        -- if Neovim/Lazy.Nvim doesn't want to build the plugin, you can do this manually by going to ~/.local/share/nvim/lazy/telescope-fzf-native.nvim and running "make" in the command line.
+        build = 'make',
+      },
     },
     config = function()
       require("telescope").setup {
